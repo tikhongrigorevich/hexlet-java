5828f0e7b390c54746bdf7994684cf5eed2d6cc5
Tikhon Pilipchuk <tikhon.pilipchuk@gmail.com>
2026-06-10 21:34:14 +0300

    update One.java file

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,7 +1,10 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        var result = StringUtils.reverse("Hexlet");
+        System.out.println(result);
     }
 }
